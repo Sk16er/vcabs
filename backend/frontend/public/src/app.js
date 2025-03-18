4e4387d461a67957fc6c46cd4115de5f7cafd5c5
shushank <shushankpawar664@gmail.com>
2025-03-18 19:15:06 +0530

    Add files via upload

---
+++ backend/frontend/public/src/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import VideoCall from "./VideoCall";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Video Call App</h1>
+      <VideoCall />
+    </div>
+  );
+}
+
+export default App;
